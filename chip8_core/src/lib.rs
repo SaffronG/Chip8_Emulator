d1145665535f660d510db06771a0284ae664ddee
SaffronGamer <saffrongamer167@gmail.com>
2024-12-10 19:00:39 -0700

    added fetch, tick, reset functions, and fontset sprites

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -3,16 +3,35 @@ pub const SCREEN_HEIGHT: usize = 32;
 const RAM_SIZE: usize = 4096;
 const NUM_REGS: usize = 16;
 const STACK_SIZE: usize = 16;
-pub struct NUM_KEYS: usize = 16;
+pub const NUM_KEYS: usize = 16;
 const START_ADDR: u16 = 0x200;
+const FONTSET_SIZE: usize = 80;
+const FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+];
 
 pub struct Emu {
     pc: u16, // Program Counter
     ram: [u8; RAM_SIZE], // Arr of u8 to be chip8's RAM
-    screen: [bool, SCREEN_WIDTH * SCREEN_HEIGHT], // Arr of Bool
+    screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT], // Arr of Bool
     v_reg: [u8; NUM_REGS], // Overflow Registers
     i_reg: u16, // Index Register (singular for R/W cmds)
-    sp: u16; // Stack Pointer
+    sp: u16, // Stack Pointer, holds the currently referenced index
     stack: [u16; STACK_SIZE],
     keys: [bool; NUM_KEYS], // Stores key presses
     dt: u8, // Delay Timer
@@ -21,26 +40,56 @@ pub struct Emu {
 
 impl Emu {
     pub fn new() -> Self {
-        Self {
+        let mut new_emu = Self {
             pc: START_ADDR,
             ram: [0; RAM_SIZE],
-            screen: [false, SCREEN_WIDTH * SCREEN_HEIGHT],
+            screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
             v_reg: [0; NUM_REGS],
             i_reg: 0,
             sp: 0,
-            stack: [0, STACK_SIZE],
-            keys: [false, NUM_KEYS],
+            stack: [0; STACK_SIZE],
+            keys: [false; NUM_KEYS],
             dt: 0,
             st: 0,
-        }
+        };
+        // LOAD THE FONT SPRITES INTO RAM USING THE FONTSET SIZE
+        new_emu.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+        new_emu
     }
 
-    fn push(&mut self, val: u16) -> u16 {
+    pub fn reset(&mut self) {
+        self.pc = START_ADDR;
+        self.ram = [0; RAM_SIZE];
+        self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+        self.v_reg = [0; NUM_REGS];
+        self.i_reg = 0;
+        self.sp = 0;
+        self.stack = [0; STACK_SIZE];
+        self.keys = [false; NUM_KEYS];
+        self.dt = 0;
+        self.sp = 0;
+        self.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+    }
+
+    fn push(&mut self, val: u16) {
         self.stack[self.sp as usize] = val;
         self.sp += 1;
     }
-    fn pop(&mut self val u16) -> u16 {
+
+    fn pop(&mut self, val: u16) -> u16 {
         self.sp -= 1;
         self.stack[self.sp as usize]
+    }
+
+    pub fn tick(&mut self) {
+        let op = self.fetch();
+    }
+
+    fn fetch(&mut self) -> u16 {
+        let higher_byte = self.ram[self.pc as usize] as u16;
+        let lower_byte: u16 = self.ram[(self.pc+1) as usize] as u16;
+        let op = (higher_byte << 8) | lower_byte;
+        self.pc += 2;
+        op
     }
 }
