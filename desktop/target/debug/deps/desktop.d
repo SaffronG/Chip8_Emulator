782cdcaef96a82aaf2614b7ac9adcd05a2a95feb
SaffronGamer <saffrongamer167@gmail.com>
2024-12-11 18:54:36 -0700

    Added window creation

--- desktop/target/debug/deps/desktop.d
+++ desktop/target/debug/deps/desktop.d
@@ -1,5 +1,5 @@
-C:\Users\chase\Documents\rust_projects\chip8_emulator\desktop\target\debug\deps\desktop.exe: src/main.rs
+C:\Users\chase\Documents\rust_projects\Chip8_Emulator\desktop\target\debug\deps\desktop.exe: src/main.rs
 
-C:\Users\chase\Documents\rust_projects\chip8_emulator\desktop\target\debug\deps\desktop.d: src/main.rs
+C:\Users\chase\Documents\rust_projects\Chip8_Emulator\desktop\target\debug\deps\desktop.d: src/main.rs
 
 src/main.rs:
